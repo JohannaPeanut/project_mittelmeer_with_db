64fe2cae7a532366ac719391a98566d1b446621d
JohannaPeanut <Johanna.k.michel@posteo.de>
2022-04-29 16:11:10 +0200

    change sentence and fetch model

--- routes/base.js
+++ routes/base.js
@@ -4,8 +4,8 @@ const dummyImage = require('../public/dummy_image1.json')
 const unirand = require('unirand')
 const { HostedModel } = require('@runwayml/hosted-models')
 
-const url ="https://landscapes-favourite-backgrounds-51351e12.hosted-models.runwayml.cloud/v1"
-const token =  "Sd04VQa2+dmmNSq0RCxFQQ=="
+const url = process.env.RUNWAY_URL
+const token = process.env.RUNWAY_KEY
 
 const router = new express.Router();
 
@@ -17,50 +17,50 @@ router.post('/landscape', (req, res, next) => {
   // simulate loading time with set time out for 1 sec
   setTimeout(function(){ 
 
-  // dev version: fetch an image from database and render view 
-  Landscape.findOne({ isUsed: false }, {}, { sort: { 'createdAt' : 1 } })
-   .then((displayedLandscape) => {
-     // render view with the fetched landscape from db
-     res.render('landscape', { displayedLandscape })
-   })
-   .catch(error => {
-     next(error)
-   })
+  // // dev version: fetch an image from database and render view 
+  // Landscape.findOne({ isUsed: false }, {}, { sort: { 'createdAt' : 1 } })
+  //  .then((displayedLandscape) => {
+  //    // render view with the fetched landscape from db
+  //    res.render('landscape', { displayedLandscape })
+  //  })
+  //  .catch(error => {
+  //    next(error)
+  //  })
   
-  //  // get random array using Pareto distribution
-  //   const randomArray = unirand.pareto(1,3).distributionSync(512)
-  //   const trunc = 0.6
+   // get random array using Pareto distribution
+    const randomArray = unirand.pareto(1,3).distributionSync(512)
+    const trunc = 0.6
   
-  //  // model authentication
-  //   const model = new HostedModel({
-  //     url: url,
-  //     token: token}) // DANGER !! - Needs to be encripted
+   // model authentication
+    const model = new HostedModel({
+      url: url,
+      token: token}) // DANGER !! - Needs to be encripted
 
-  //   // create input object for model
-  //   const inputs = {
-  //     "z": randomArray, 
-  //     "truncation": trunc}
+    // create input object for model
+    const inputs = {
+      "z": randomArray, 
+      "truncation": trunc}
     
-  //   // run model with above data
-  //   model.query(inputs)     
-  // .then((newLandscape)=> {
-  //   console.log('model ran')
-  //   // store new document in the database with image property landscape file
-  //   Landscape.create({ isUsed: false, image: newLandscape.image })
-  // })
-  // .catch(error => {
-  //   next(error)
-  // })
+    // run model with above data
+    model.query(inputs)     
+  .then((newLandscape)=> {
+    console.log('model ran')
+    // store new document in the database with image property landscape file
+    Landscape.create({ isUsed: false, image: newLandscape.image })
+  })
+  .catch(error => {
+    next(error)
+  })
   
-  // // fetch oldet document from the db collection that is not already used and update it to isUsed: true
-  // Landscape.findOneAndUpdate({ isUsed: false }, { isUsed: true}, { sort: { 'createdAt' : 1 } })
-  // .then((displayedLandscape) => {
-  //   // render view with the fetched landscape from db
-  //   res.render('landscape', { displayedLandscape })
-  // })
-  // .catch(error => {
-  //   next(error)
-  // })
+  // fetch oldet document from the db collection that is not already used and update it to isUsed: true
+  Landscape.findOneAndUpdate({ isUsed: false }, { isUsed: true}, { sort: { 'createdAt' : 1 } })
+  .then((displayedLandscape) => {
+    // render view with the fetched landscape from db
+    res.render('landscape', { displayedLandscape })
+  })
+  .catch(error => {
+    next(error)
+  })
 
 }, 1000) 
 });
